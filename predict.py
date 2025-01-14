17ebfd1a53eaa5efe7e5f1b2a5f21d7c71e0368b
Paul <133292312+Chlmpanzy@users.noreply.github.com>
2025-01-14 18:20:18 -0500

    Added Fake Predictor for Backend to use for testing

---
+++ predict.py
@@ -0,0 +1,10 @@
+
+class Redo():
+    def __init__(self):
+        pass
+
+    def getTeam():
+        return ['Bucks', 'Nuggets', 'Pelicans', 'Nets', 'Knicks', '76ers', 'Pacers', 'Cavaliers', 'Warriors', 'Jazz', 'Mavericks', 'Wizards', 'Hornets', 'Kings', 'Timberwolves', 'Grizzlies', 'Bulls', 'Raptors', 'Pistons', 'Rockets', 'Magic', 'Thunder', 'Clippers', 'Spurs', 'Heat', 'Lakers', 'Suns', 'Celtics', 'Trail Blazers', 'Hawks']
+
+    def predict(sportType, team1, team2, betType):
+        return "Predicting..."
