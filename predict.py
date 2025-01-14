594481fac290a42cd89c79c82435a95013313ac9
Paul <133292312+Chlmpanzy@users.noreply.github.com>
2025-01-14 18:26:50 -0500

    Fixed Bugs

--- predict.py
+++ predict.py
@@ -3,8 +3,8 @@ class Redo():
     def __init__(self):
         pass
 
-    def getTeam():
+    def getTeams(self):
         return ['Bucks', 'Nuggets', 'Pelicans', 'Nets', 'Knicks', '76ers', 'Pacers', 'Cavaliers', 'Warriors', 'Jazz', 'Mavericks', 'Wizards', 'Hornets', 'Kings', 'Timberwolves', 'Grizzlies', 'Bulls', 'Raptors', 'Pistons', 'Rockets', 'Magic', 'Thunder', 'Clippers', 'Spurs', 'Heat', 'Lakers', 'Suns', 'Celtics', 'Trail Blazers', 'Hawks']
 
-    def predict(sportType, team1, team2, betType):
+    def predict(self, team1, team2, betType):
         return "Predicting..."
