1e9a1a307ad5fcb58ba7af7078b5ac75b6329fe4
Paul <133292312+Chlmpanzy@users.noreply.github.com>
2025-01-19 19:44:22 -0500

    Linked front and back end with "gettting future games" logic

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect, url_for
+from classes import Predictor, Scraper 
+
+app = Flask(__name__)
+
+predictor = Predictor()
+scraper = Scraper()
+
+@app.route("/")
+def main():
+    teams = predictor.getTeams()
+    upcomingGames = scraper.getUpcoming(1)
+    return render_template("frontend.html", teams = teams, upcomingGames = upcomingGames)
+
+@app.route("/predict", methods=["POST"])
+def prediction():
+    team1 = request.form.get("team1")
+    team2 = request.form.get("team2")
+    sport = request.form.get("sport")
+    bet = request.form.get("bet")
+    if not all([team1, team2, sport, bet]):
+        return {"error": "All fields are required!"}, 400
+
+    if team1 == team2:
+        return {"error": "Team 1 and Team 2 cannot be the same!"}, 400
+
+    prediction = predictor.predict(sport = sport, team1=team1, team2=team2, betType=bet)
+
+    return {"prediction": prediction}, 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
